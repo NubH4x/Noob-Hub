932a593361c5c5892b94f31892fb7594dfadb197
NubH4x <78987144+NubH4x@users.noreply.github.com>
2021-03-12 09:20:25 -0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,19 @@
---noob hub is currently not done, so please wait. we're adding more games!
+--arsenal
+if game.PlaceId==286090429 then do
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/CB%3ARO.lua'))()
+
+--cb:ro
+if game.PlaceId==301549746 then do
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/CB%3ARO.lua'))()
+
+--unit classified
+if game.PlaceId==4292776423 then do
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/CB%3ARO.lua'))()
+
+--unit 1968
+if game.PlaceId==746820961 then do
+                loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/CB%3ARO.lua'))()
+
+--typical colors 2
+if game.PlaceId==328028363 then do
+                    loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/CB%3ARO.lua'))()
