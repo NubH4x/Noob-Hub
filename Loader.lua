a40a819aa42f085d115a2eacec4b4b287ccc9be0
NubH4x <78987144+NubH4x@users.noreply.github.com>
2021-03-12 09:26:09 -0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,19 +1,15 @@
---arsenal
 if game.PlaceId==286090429 then do
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/Arsenal.lua'))()
-
---cb:ro
+loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/Arsenal.lua'))()
+        
 if game.PlaceId==301549746 then do
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/CB%3ARO.lua'))()
-
---unit classified
+loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/CB%3ARO.lua'))()
+                
 if game.PlaceId==4292776423 then do
-            loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/CB%3ARO.lua'))()
-
---unit 1968
+loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/Unit%3A%20Classified.lua'))()
+                        
 if game.PlaceId==746820961 then do
-                loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/CB%3ARO.lua'))()
-
---typical colors 2
+loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/Unit%3A%201968.lua'))()
+                                
 if game.PlaceId==328028363 then do
-                    loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/CB%3ARO.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/Typical%20Colors%202.lua'))()
+                                        
