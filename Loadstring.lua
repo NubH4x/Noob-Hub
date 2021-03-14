fb0878100434a6eb76b2c8511de86f9fb9e866b2
NubH4x <78987144+NubH4x@users.noreply.github.com>
2021-03-13 19:17:10 -0800

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/Loader.lua', true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/Loader.lua'))()
