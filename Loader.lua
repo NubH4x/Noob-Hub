8d6340f4c38248b8f6e42ca715f97412731aad92
NubH4x <78987144+NubH4x@users.noreply.github.com>
2021-03-13 19:09:59 -0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,15 +1,14 @@
-if game.PlaceId==286090429 then do
-loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/Arsenal.lua'))()
+if game.PlaceId == 286090429 then
+loadstring(game:HttpGet(''))()
         
-if game.PlaceId==301549746 then do
-loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/CB%3ARO.lua'))()
+if game.PlaceId == 301549746 then
+loadstring(game:HttpGet(''))()
                 
-if game.PlaceId==4292776423 then do
-loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/Unit%3A%20Classified.lua'))()
+if game.PlaceId == 4292776423 then
+loadstring(game:HttpGet(''))()
                         
-if game.PlaceId==746820961 then do
-loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/Unit%3A%201968.lua'))()
+if game.PlaceId == 746820961 then
+loadstring(game:HttpGet(''))()
                                 
-if game.PlaceId==328028363 then do
-loadstring(game:HttpGet('https://raw.githubusercontent.com/NubH4x/Noob-Hub/main/Typical%20Colors%202.lua'))()
-                                        
+if game.PlaceId == 328028363 then
+loadstring(game:HttpGet(''))()
